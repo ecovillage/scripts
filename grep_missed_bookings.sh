f2a87b32aa0c75f955adb54c72d73755a077fe4a
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-01-08 12:51:38 +0100

    fix awk usage: dont escape literal '(', use regex for -F

--- grep_missed_bookings.sh
+++ grep_missed_bookings.sh
@@ -35,7 +35,7 @@ grep -n -B $window "$searchstr" "$1" | \
   # now search backwards to get last appearance of $regstr
   tac | grep "$regstr" | tac | \
   # cut out VALUES field of JSON string | delete quotes and braces | delete trailing ')'
-  awk -F 'VALUES \(' '{ print $2 }' | tr -d "\'\[\]" | awk -F '\)$' '{print $1}' | \
+  awk -F 'VALUES [(]' '{ print $2 }' | tr -d "\'\[\]" | awk -F '[)]$' '{print $1}' | \
   # printf fields seperated by ','. Print 'date'-field first
   awk -F ',' '{ printf $NF" "; for(i=1; i<NF; i++ ){ printf $i }; printf "\n"}' | \
   # cut leading :blank: and look for double lines skipping leading 'date'-field
