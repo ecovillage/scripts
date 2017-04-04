441a7a385dc6b6e93057e26b9b24993aaff06d82
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-04-04 09:49:09 +0200

    use hostname instead of ip in piwik script

--- piwik_consume_logfile_idsite1_siebenlinden.sh
+++ piwik_consume_logfile_idsite1_siebenlinden.sh
@@ -20,8 +20,8 @@ ACCESS_LOG_FILE=$(readlink -m "$1")
 
 sudo chown www-data "$ACCESS_LOG_FILE"
 
-sudo -u www-data python /var/www/piwik/misc/log-analytics/import_logs.py --url http://192.168.1.166/piwik --idsite=1 $ACCESS_LOG_FILE
-cd /var/www/piwik/ && sudo -u www-data ./console core:archive --force-idsites=1 --force-all-periods=315576000 --force-date-last-n=1000 --url='http://192.168.1.166/piwik'
+sudo -u www-data python /var/www/piwik/misc/log-analytics/import_logs.py --url http://piwik.7l/piwik --idsite=1 $ACCESS_LOG_FILE
+cd /var/www/piwik/ && sudo -u www-data ./console core:archive --force-idsites=1 --force-all-periods=315576000 --force-date-last-n=1000 --url='http://piwik.7l/piwik'
 sudo rm $ACCESS_LOG_FILE
 
 exit 0
